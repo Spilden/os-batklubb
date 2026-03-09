719b9a9d029b0d4ba33f0084f338e03a0e870603
spilden <mr.spilde@gmail.com>
2026-03-09 11:37:42 +0100

    endret til cursor pointer for enda mer synlighet at det er en knapp

--- src/components/BaseButton.tsx
+++ src/components/BaseButton.tsx
@@ -8,8 +8,8 @@ type BaseButtonProps = {
 
 export default function BaseButton({ children, onClick, variant = 'primary' }: BaseButtonProps) {
   const variants = {
-    primary: 'bg-primary text-surface border border-transparent shadow-lg hover:bg-ocean hover:border hover:border-ocean',
-    secondary: 'border border-primary text-primary shadow-lg hover:bg-ocean hover:text-surface hover:border-ocean',
+    primary: 'bg-primary text-surface border border-transparent shadow-lg cursor-pointer hover:bg-ocean hover:border hover:border-ocean',
+    secondary: 'border border-primary text-primary shadow-lg cursor-pointer hover:bg-ocean hover:text-surface hover:border-ocean',
     text: 'underline underline-offset-4 text-primary cursor-pointer text-shadow-lg hover:text-text',
   }
   return (
