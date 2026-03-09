94382a882ced7e4688a945993f4d79858f255713
spilden <mr.spilde@gmail.com>
2026-03-09 11:44:13 +0100

    lagt til className som prop i knappene slik at de kan styles seperat i tillegg til base stylen

--- src/components/BaseButton.tsx
+++ src/components/BaseButton.tsx
@@ -4,9 +4,10 @@ type BaseButtonProps = {
   children: ReactNode;
   onClick?: () => void;
   variant?: 'primary' | 'secondary' | 'text';
+  className?: string;
 }
 
-export default function BaseButton({ children, onClick, variant = 'primary' }: BaseButtonProps) {
+export default function BaseButton({ children, onClick, variant = 'primary', className }: BaseButtonProps) {
   const variants = {
     primary: 'bg-primary text-surface border border-transparent shadow-lg cursor-pointer hover:bg-ocean hover:border hover:border-ocean',
     secondary: 'border border-primary text-primary shadow-lg cursor-pointer hover:bg-ocean hover:text-surface hover:border-ocean',
@@ -15,7 +16,7 @@ export default function BaseButton({ children, onClick, variant = 'primary' }: B
   return (
     <button
     onClick={onClick}
-    className={`${variants[variant]} px-6 py-3 rounded-lg text-sm uppercase transition-colors`}
+    className={`${variants[variant]} ${className}  px-6 py-3 rounded-lg text-sm uppercase transition-colors`}
     >
       {children}
     </button>
