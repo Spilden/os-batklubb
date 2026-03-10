3c19c3dd8d8f33afbb0034b3bbee121cefbbc5e4
spilden <mr.spilde@gmail.com>
2026-03-10 15:56:30 +0100

    Endret slik at NewsCard bare displayer ett kort, og tar bare en artikkel inn som props.

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -3,34 +3,24 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 type NewsCardProps = {
-  articles: News[]
+  article: News
 }
 
-export default function NewsCard({ articles }: NewsCardProps) {
+export default function NewsCard({ article }: NewsCardProps) {
+  const image = article.image as Media
   return (
-    <div className="flex flex-col gap-y-6 m-4">
-      {articles.map((article) => {
-        const image = article.image as Media
-
-        return (
-          <Link
-            key={article.id}
-            href={`/news/${article.slug}`}
-          >
-            <article
-              className="flex bg-background rounded-lg p-4 shadow-lg w-full gap-4 cursor-pointer hover:shadow-ocean"
-            >
-              <div className="relative w-3/5 aspect-video flex-shrink-0">
-                <Image src={image.url!} alt={image.alt!} fill className="rounded-lg object-cover" />
-              </div>
-              <div className="flex flex-col gap-y-4 h-full mx-4">
-                <h2 className="text-3xl text-text font-display text-center">{article.title}</h2>
-                <p className="text-text-muted whitespace-pre-wrap">{article.excerpt}</p>
-              </div>
-            </article>
-          </Link>
-        )
-      })}
-    </div>
+    <Link
+      key={article.id}
+      href={`/news/${article.slug}`}
+      className="flex bg-surface rounded-lg p-4 shadow-lg w-full gap-4 cursor-pointer hover:shadow-ocean"
+    >
+      <div className="relative w-3/5 aspect-video flex-shrink-0">
+        <Image src={image.url!} alt={image.alt!} fill className="rounded-lg object-cover" />
+      </div>
+      <div className="flex flex-col gap-y-4 h-full mx-4">
+        <h2 className="text-3xl text-text font-display text-center">{article.title}</h2>
+        <p className="text-text-muted whitespace-pre-wrap">{article.excerpt}</p>
+      </div>
+    </Link>
   )
 }
