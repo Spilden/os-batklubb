968c48859f6dbc37b1cdc71963260742cad4ae5c
Stevan M. Vågen <stevanvaagen@gmail.com>
2026-03-10 12:11:01 +0100

    Lagt til nav element for desktop

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,28 @@ export default function Header() {
         <Link href="/">
           <Image src="/obk_logo.svg" alt="Os Båtklubb" width={120} height={60} />
         </Link>
+
+        {/*Desktop navigasjon*/}
+        <ul>
+          <li>
+            <Link href="/">Hjem</Link>
+          </li>
+          <li>
+            <Link href="/news">Nyheter</Link>
+          </li>
+          <li>
+            <Link href="/about">Om Båtklubben</Link>
+          </li>
+          <li>
+            <Link href="/guest-marina">Gjestehavn</Link>
+          </li>
+          <li>
+            <Link href="/contact">Kontakt Oss</Link>
+          </li>
+          <li>
+            <Link href="/log-in">Logg Inn</Link>
+          </li>
+        </ul>
       </nav>
     </header>
   )
