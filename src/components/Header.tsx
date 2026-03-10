5ef1070a556794fb369625856b218bc4c2864633
Stevan M. Vågen <stevanvaagen@gmail.com>
2026-03-10 12:42:44 +0100

    Stylet navbar for desktop og tatt i bruk button komponent

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,33 +1,46 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import BaseButton from '@/components/BaseButton'
 
 export default function Header() {
   return (
-    <header>
-      <nav>
+    <header className="bg-white p-8 shadow-lg">
+      <nav className="flex items-center justify-between">
         <Link href="/">
           <Image src="/obk_logo.svg" alt="Os Båtklubb" width={120} height={60} />
         </Link>
 
         {/*Desktop navigasjon*/}
-        <ul>
+        <ul className="flex gap-2">
           <li>
-            <Link href="/">Hjem</Link>
+            <Link href="/">
+              <BaseButton>Hjem</BaseButton>
+            </Link>
           </li>
           <li>
-            <Link href="/news">Nyheter</Link>
+            <Link href="/news">
+              <BaseButton>Nyheter</BaseButton>
+            </Link>
           </li>
           <li>
-            <Link href="/about">Om Båtklubben</Link>
+            <Link href="/about">
+              <BaseButton>Om Båtklubben</BaseButton>
+            </Link>
           </li>
           <li>
-            <Link href="/guest-marina">Gjestehavn</Link>
+            <Link href="/guest-marina">
+              <BaseButton>Gjestehavn</BaseButton>
+            </Link>
           </li>
           <li>
-            <Link href="/contact">Kontakt Oss</Link>
+            <Link href="/contact">
+              <BaseButton>Kontakt Oss</BaseButton>
+            </Link>
           </li>
           <li>
-            <Link href="/log-in">Logg Inn</Link>
+            <Link href="/log-in">
+              <BaseButton variant="secondary">Logg Inn</BaseButton>
+            </Link>
           </li>
         </ul>
       </nav>
