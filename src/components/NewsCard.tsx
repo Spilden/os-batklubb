a3c0ebee09f9b326c8c8c0c15f7bc38d8c5a6722
Stevan M. Vågen <stevanvaagen@gmail.com>
2026-03-11 11:43:34 +0100

    Håndter manglende bilde i NewsCard

--- src/components/NewsCard.tsx
+++ src/components/NewsCard.tsx
@@ -7,16 +7,23 @@ type NewsCardProps = {
 }
 
 export default function NewsCard({ article }: NewsCardProps) {
-  const image = article.image as Media
+  const image = article.image as Media | undefined
   return (
     <Link
       key={article.id}
       href={`/news/${article.slug}`}
       className="flex bg-surface rounded-lg p-4 shadow-lg w-full gap-4 cursor-pointer hover:shadow-ocean"
     >
-      <div className="relative w-3/5 aspect-video flex-shrink-0">
-        <Image src={image.url!} alt={image.alt!} fill className="rounded-lg object-cover" />
-      </div>
+      {image?.url && (
+        <div className="relative w-3/5 aspect-video shrink-0">
+          <Image
+            src={image.url}
+            alt={image.alt ?? article.title}
+            fill
+            className="rounded-lg object-cover"
+          />
+        </div>
+      )}
       <div className="flex flex-col gap-y-4 h-full mx-4">
         <h2 className="text-3xl text-text font-display text-center">{article.title}</h2>
         <p className="text-text-muted whitespace-pre-wrap">{article.excerpt}</p>
