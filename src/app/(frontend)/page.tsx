0df0c091db8178e3a07f36579a185a697e6c6a2a
Stevan M. Vågen <stevanvaagen@gmail.com>
2026-03-18 17:25:14 +0100

    laget enkel seksjon for partnere-sponsor

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,24 +1,53 @@
 import { getPayload } from 'payload'
 import React from 'react'
-
 import config from '@/payload.config'
 import BaseButton from '@/components/BaseButton'
+import Image from 'next/image'
+import { Media } from '@/payload-types'
 
 export default async function HomePage() {
   const payloadConfig = await config
   const payload = await getPayload({ config: payloadConfig })
+  const { docs: partners } = await payload.find({
+    collection: 'partners',
+    depth: 1,
+  })
 
   return (
-    <div className="flex pt-2 items-center justify-center">
-      <div className="bg-white rounded-xl p-8 shadow-lg w-96">
-        <h1 className="font-display text-3xl text-text">Her er en tittel</h1>
-        <p className="text-text-muted mt-2">Her er en beskrivelse.</p>
-        <div>
-          <BaseButton>primary</BaseButton>
-          <BaseButton variant="secondary">Secondary</BaseButton>
-          <BaseButton variant="text">textbutton</BaseButton>
+    <>
+      <section>
+        <div className="flex pt-2 items-center justify-center">
+          <div className="bg-white rounded-xl p-8 shadow-lg w-96">
+            <h1 className="font-display text-3xl text-text">Her er en tittel</h1>
+            <p className="text-text-muted mt-2">Her er en beskrivelse.</p>
+            <div>
+              <BaseButton>primary</BaseButton>
+              <BaseButton variant="secondary">Secondary</BaseButton>
+              <BaseButton variant="text">textbutton</BaseButton>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </section>
+      <section>
+        {partners.map((partner) => {
+          const partnerLogo = partner.image as Media
+
+          return (
+            <div key={partner.id}>
+              <h2>{partner.title}</h2>
+              {partnerLogo?.url && (
+                <Image
+                  src={partnerLogo.url}
+                  alt={partner.title}
+                  width={160}
+                  height={80}
+                  className="object-contain"
+                />
+              )}
+            </div>
+          )
+        })}
+      </section>
+    </>
   )
 }
