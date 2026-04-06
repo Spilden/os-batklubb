81c29e1e478dd6eaa770ce0dbd7d97c3488b0568
spilden <mr.spilde@gmail.com>
2026-04-06 21:44:07 +0200

    laget en modal for data på gjestehavn siden

---
+++ src/globals/GuestMarina.ts
@@ -0,0 +1,46 @@
+﻿import { GlobalConfig } from 'payload'
+
+export const GuestMarina: GlobalConfig = {
+  slug: 'guestMarina',
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+      admin: {
+        placeholder: 'Overskrift',
+      },
+    },
+    {
+      name: 'content',
+      type: 'richText',
+      required: true,
+      admin: {
+        description:
+          'Dette feltet er hovedinnholdet som forklarer gjester om gjestehavnen. Formatering her blir brukt på siden'
+      },
+    },
+    {
+      name: 'image',
+      type: 'upload',
+      relationTo: 'media',
+      required: true,
+    },
+    {
+      name: 'price',
+      type: 'number',
+      required: true,
+      admin: {
+        placeholder: 'døgnpris på gjestehavnen',
+      },
+    },
+    {
+      name: 'paymentInfo',
+      type: 'text',
+      required: true,
+      admin:{
+        description: 'Legg inn informasjon om betaling av døgnpris'
+      },
+    },
+  ],
+}
