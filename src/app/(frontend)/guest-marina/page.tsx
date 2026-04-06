0dbb5f7e16ff87f1d5c5e807e4b466b2a7daa84f
spilden <mr.spilde@gmail.com>
2026-04-06 20:32:43 +0200

    Opprettet gjestehavn siden med styling lik nyhetssiden

---
+++ src/app/(frontend)/guest-marina/page.tsx
@@ -0,0 +1,13 @@
+﻿import React from 'react'
+
+export default function GuestMarinaPage(){
+  return (
+    <div className="bg-sage rounded-xl shadow-lg w-full">
+      <div className="rounded-t-xl bg-ocean p-4">
+        <h1 className="text-text text-2xl font-bold text-center">Gjestehavnen</h1>
+      </div>
+      <div className="flex flex-col gap-y-6 p-6">
+      </div>
+    </div>
+  )
+}
