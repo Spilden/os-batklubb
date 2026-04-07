0ac679d48e9cc5f583951698e1f97e1c8f053e19
spilden <mr.spilde@gmail.com>
2026-04-07 13:30:24 +0200

    Lagt til tailwind typography styling for next RichText

--- src/app/(frontend)/guest-marina/page.tsx
+++ src/app/(frontend)/guest-marina/page.tsx
@@ -1,12 +1,27 @@
 ﻿import React from 'react'
+import { getPayload } from 'payload'
+import payloadConfig from '@payload-config'
+import { RichText } from '@payloadcms/richtext-lexical/react'
+import { imageGuard } from '@/utils/ImageGuard'
+import Image from 'next/image'
 
-export default function GuestMarinaPage(){
+export default async function GuestMarinaPage(){
+  const payload = await getPayload({ config: payloadConfig })
+
+  const data = await payload.findGlobal({slug: 'guestMarina'})
+  console.log(JSON.stringify(data.content))
+
+  const image = imageGuard(data.image)
+
   return (
     <div className="bg-sage rounded-xl shadow-lg w-full">
       <div className="rounded-t-xl bg-ocean p-4">
-        <h1 className="text-text text-2xl font-bold text-center">Gjestehavnen</h1>
+        <h1 className="text-text text-2xl font-bold text-center">{data.title}</h1>
       </div>
       <div className="flex flex-col gap-y-6 p-6">
+        {image && (
+          <Image src={image.url} alt={image.alt} width={image.width} height={image.height} />)}
+        <RichText className="prose lg:prose-xl" data={data.content} />
       </div>
     </div>
   )
