fd8d743fbb65b731e58ad3e8cb241a743e8743bc
Vaagens <stevanvaagen@gmail.com>
2026-04-13 20:34:21 +0200

    fjernet button eksempel seksjon

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,7 +1,6 @@
 import { getPayload } from 'payload'
 import React from 'react'
 import config from '@/payload.config'
-import BaseButton from '@/components/BaseButton'
 import PartnerCard from '@/components/PartnerCard'
 
 export default async function HomePage() {
@@ -13,19 +12,6 @@ export default async function HomePage() {
 
   return (
     <>
-      <section>
-        <div className="flex pt-2 items-center justify-center">
-          <div className="bg-white rounded-xl p-8 shadow-lg w-96">
-            <h1 className="font-display text-3xl text-text">Her er en tittel</h1>
-            <p className="text-text-muted mt-2">Her er en beskrivelse.</p>
-            <div>
-              <BaseButton>primary</BaseButton>
-              <BaseButton variant="secondary">Secondary</BaseButton>
-              <BaseButton variant="text">textbutton</BaseButton>
-            </div>
-          </div>
-        </div>
-      </section>
 
       {partners.length > 0 && (
         <section className="bg-sage rounded-xl shadow-lg w-full mt-4">
