7c991eb037018ccc202e70c446556757af37b9ec
Vaagens <stevanvaagen@gmail.com>
2026-04-18 00:49:51 +0200

    Istedet laget flere roller i user

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -7,7 +7,24 @@ export const Users: CollectionConfig = {
   },
   auth: true,
   fields: [
-    // Email added by default
-    // Add more fields as needed
+    {
+      name: 'name',
+      type: 'text',
+      label: 'Navn',
+      required: true,
+    },
+    {
+      name: 'roles',
+      type: 'select',
+      label: 'Roller',
+      hasMany: true,
+      required: true,
+      defaultValue: ['member'],
+      options: [
+        { label: 'Admin', value: 'admin' },
+        { label: 'Medlem', value: 'member' },
+        { label: 'Nyhetsredaktør', value: 'news_admin' },
+      ],
+    },
   ],
 }
