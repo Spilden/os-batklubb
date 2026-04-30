f7e2fe33a297535295cb6f095540901ba9e92b1a
spilden <mr.spilde@gmail.com>
2026-04-30 22:41:02 +0200

    feat: endret slik at man kan velge mellom ukentlig og månedlig visning

--- src/components/BaseCalendar.tsx
+++ src/components/BaseCalendar.tsx
@@ -2,6 +2,7 @@
 
 import FullCalendar from '@fullcalendar/react'
 import timeGridPlugin from '@fullcalendar/timegrid'
+import dayGridPlugin from '@fullcalendar/daygrid'
 import nbLocale from '@fullcalendar/core/locales/nb'
 import type { CalendarOptions } from '@fullcalendar/core'
 
@@ -9,18 +10,23 @@ export function BaseCalendar(props: CalendarOptions) {
   return (
     <div className="max-w-4xl mx-auto p-8 bg-surface rounded-2xl shadow-[0_6px_32px_rgba(100,70,40,0.12)] border border-border">
       <FullCalendar
-        plugins={[timeGridPlugin]}
-        initialView="timeGridWeek"
+        plugins={[timeGridPlugin, dayGridPlugin]}
+        initialView="dayGridMonth"
+        headerToolbar={{
+          left: 'dayGridMonth,timeGridWeek',
+          center: 'title',
+          right: 'today prev,next'
+        }}
         locale={nbLocale}
         firstDay={1}
         height="auto"
-        slotMinTime="07:00:00"
-        slotMaxTime="22:00:00"
-        slotDuration="01:00:00"
+        slotMinTime="00:00:00"
+        slotMaxTime="24:00:00"
+        slotDuration="02:00:00"
         slotLabelFormat={{ hour: '2-digit', minute: '2-digit', hour12: false }}
         allDaySlot={false}
-        nowIndicator={true}
-        buttonText={{ today: 'I dag' }}
+        nowIndicator={false}
+        buttonText={{ today: 'I dag', month: 'Måned', week: 'Uke' }}
         {...props}
       />
     </div>
