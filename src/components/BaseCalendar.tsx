63f3269685a3a0b53e1ed757226f8839bc135e00
spilden <mr.spilde@gmail.com>
2026-04-30 20:44:59 +0200

    feat: laget komponent for en base kalender, denne importerer fullcalendar fra react

---
+++ src/components/BaseCalendar.tsx
@@ -0,0 +1,27 @@
+﻿'use client'
+
+import FullCalendar from '@fullcalendar/react'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import nbLocale from '@fullcalendar/core/locales/nb'
+
+export function BaseCalendar(props: any) {
+  return (
+    <div className="max-w-4xl mx-auto p-8 bg-[#FDFAF6] rounded-2xl shadow-[0_6px_32px_rgba(100,70,40,0.12)] border border-[#E8DDD0]">
+      <FullCalendar
+        plugins={[timeGridPlugin]}
+        initialView="timeGridWeek"
+        locale={nbLocale}
+        firstDay={1}
+        height="auto"
+        slotMinTime="07:00:00"
+        slotMaxTime="22:00:00"
+        slotDuration="01:00:00"
+        slotLabelFormat={{ hour: '2-digit', minute: '2-digit', hour12: false }}
+        allDaySlot={false}
+        nowIndicator={true}
+        buttonText={{ today: 'I dag' }}
+        {...props}
+      />
+    </div>
+  )
+}
