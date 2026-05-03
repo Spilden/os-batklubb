7f14f5de4ef5a093114b61fc0991bba3320fd0aa
Vaagens <stevanvaagen@gmail.com>
2026-05-03 15:14:50 +0200

    Reset passord side overskrift og struktur

---
+++ src/app/(frontend)/reset-password/page.tsx
@@ -0,0 +1,10 @@
+export default function resetPasswordPage() {
+  return (
+    <div className="bg-sage rounded-xl shadow-lg w-full max-w-xl mx-auto">
+      <div className="rounded-t-xl bg-ocean p-4">
+        <h1 className="text-text text-2xl font-bold text-center">Tilbakestill passord</h1>
+      </div>
+      <div className="p-6"></div>
+    </div>
+  )
+}
