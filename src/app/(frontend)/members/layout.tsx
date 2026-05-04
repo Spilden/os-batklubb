aabf13ae3ce30cf56b34b802374e7b9c4d4dbc9f
spilden <mr.spilde@gmail.com>
2026-05-05 01:55:44 +0200

    feat: Layout for medlemssiden, laget for å vise sidebar, med navigering på alle sidene som er under medlem

---
+++ src/app/(frontend)/members/layout.tsx
@@ -0,0 +1,19 @@
+﻿import { NavButton } from '@/components/NavButton'
+import React from 'react'
+
+export default function MembersLayout(props: { children: React.ReactNode }) {
+  const { children } = props
+
+  return (
+    <div className="flex min-h-screen gap-4">
+      <aside className="w-sm bg-red-500 self-stretch -ml-4 -mt-4 -mb-4 ">
+        <div className="flex flex-col">
+          <NavButton href="/members">Medlemsside</NavButton>
+          <NavButton href="/members/slipp">Slipp</NavButton>
+        </div>
+      </aside>
+
+      <div className="flex-1">{children}</div>
+    </div>
+  )
+}
