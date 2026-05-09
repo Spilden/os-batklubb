65babf6ec5494bd1a94f48e1f7c32ab6e0c3467a
spilden <mr.spilde@gmail.com>
2026-05-09 10:37:48 +0200

    feat: satt opp en base modell av et kort

--- src/app/(frontend)/members/page.tsx
+++ src/app/(frontend)/members/page.tsx
@@ -2,6 +2,7 @@ import { getPayload } from 'payload'
 import config from '@payload-config'
 import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
+import { JSX } from 'react'
 
 export default async function MembersPage() {
   const payload = await getPayload({ config })
@@ -12,6 +13,25 @@ export default async function MembersPage() {
     <div className="w-full">
       <h1 className="font-display text-text text-3xl font-bold text-center pb-4">Velkommen {user.name}</h1>
       <p className="text-primary text-xl text-center">Dette vil være din side med mer informasjon på veg</p>
+    </div>
+  )
+}
+
+type BaseCardProps = {
+  className?: string
+  title?: string
+  content?: string
+  footer?: string
+  children?: JSX.Element
+}
+
+export function BaseCard({ title, content, footer, className, children}: BaseCardProps) {
+  return (
+    <div className={`bg-surface p-4 rounded-xl w-full pl-8 ${className}`}>
+      <h2 className="text-text-muted font-display italic">{title}</h2>
+      <p>{content}</p>
+      {children}
+      <p>{footer}</p>
     </div>
   )
 }
