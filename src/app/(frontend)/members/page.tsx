251ef470fa1d37a564af051992328983374d2646
spilden <mr.spilde@gmail.com>
2026-05-09 10:38:29 +0200

    feat: henter tidevannsdata fra kartverket

--- src/app/(frontend)/members/page.tsx
+++ src/app/(frontend)/members/page.tsx
@@ -9,6 +9,23 @@ export default async function MembersPage() {
   const { user } = await payload.auth({ headers: await headers() })
   if (!user) redirect('/admin/login')
 
+  const response = await fetch(
+    'https://vannstand.kartverket.no/tideapi.php?lat=60.1529229&lon=5.4455994&fromtime=2026-05-08T00%3A00&totime=2026-05-10T00%3A00&datatype=tab&refcode=cd&place=&file=&lang=nn&interval=10&dst=0&tzone=&tide_request=locationdata',
+  )
+  const text = await response.text()
+  const currentTime = new Date()
+
+  const result = [
+    ...text.matchAll(/<waterlevel value="([\d.]+)" time="([^"]+)" flag="([^"]+)"/g),
+  ].map((m) => ({
+    verdi: parseFloat(m[1]),
+    tid: new Date(m[2]),
+    flag: m[3],
+  }))
+  const nextTide = result.find((t) => t.tid > currentTime)
+
+  const highLowWater = nextTide?.flag === 'high' ? 'høyvann' : 'lavvann'
+
   return (
     <div className="w-full">
       <h1 className="font-display text-text text-3xl font-bold text-center pb-4">Velkommen {user.name}</h1>
