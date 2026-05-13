1369f9adfa70e21e4aea3be9fc9648ea12b68c8a
Stevan M. Vågen <stevanvaagen@gmail.com>
2026-05-13 22:12:22 +0200

    feat: lagt til kalender på klubbhussiden

--- src/app/(frontend)/members/clubhouse/page.tsx
+++ src/app/(frontend)/members/clubhouse/page.tsx
@@ -2,6 +2,7 @@ import { getPayload } from 'payload'
 import config from '@payload-config'
 import { headers } from 'next/headers'
 import { redirect } from 'next/navigation'
+import { ClubhouseCalendar } from '@/components/ClubhouseCalendar'
 
 
 export default async function ClubhousePage() {
@@ -16,7 +17,7 @@ export default async function ClubhousePage() {
   return (
     <>
       <h1 className="text-text font-display text-center text-3xl p-10">Booking av klubbhuset</h1>
-      {/*<SlippCalendar currentUser={user} initialRequests={requests} settings={settings} />*/}
+      <ClubhouseCalendar currentUser={user} initialRequests={requests}/>
       <p className="text-text text-center text-xl p-10">
         Klikk på dagen du vil booke eller dra over de dagene/timene du ønsker å booke slipp.
       </p>
