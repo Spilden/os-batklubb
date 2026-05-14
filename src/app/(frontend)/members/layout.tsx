13aa2c7b79aaf47c559f20d34e6e70a98c9bfeee
Martin <mr.spilde@gmail.com>
2026-05-14 19:12:37 +0200

    fix: juster sidemenyen for å håndtere sticky header

--- src/app/(frontend)/members/layout.tsx
+++ src/app/(frontend)/members/layout.tsx
@@ -6,7 +6,7 @@ export default function MembersLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
-    <div className="flex flex-col lg:flex-row min-h-screen w-full gap-4">
+    <div className="flex flex-col lg:flex-row w-full gap-4 min-h-[calc(100vh-296px)]">
       <nav className="flex lg:hidden">
         <Link href="/members" className="flex-1 text-center p-3 text-sm">
           Medlemsside
@@ -18,8 +18,8 @@ export default function MembersLayout(props: { children: React.ReactNode }) {
           Klubbhus
         </Link>
       </nav>
-      <aside className="hidden lg:block w-xs bg-surface self-stretch -ml-4 -mt-4 -mb-4 z-50">
-        <div className="flex flex-col">
+      <aside className="hidden lg:block w-xs bg-surface self-stretch -ml-4 -mt-4 -mb-4 z-10">
+        <div className="flex flex-col fixed w-xs top-[123] pt-8">
           <NavButton href="/members">Medlemsside</NavButton>
           <NavButton href="/members/slipp">Slipp</NavButton>
           <NavButton href="/members/clubhouse">Klubbhus</NavButton>
