b8f88e7b227713e0350a3e16f817c1211a3bff43
Vaagens <stevanvaagen@gmail.com>
2026-09-01 20:36:07 +0200

    lagt til images slik at bilder vises med trycloudflare, endret værstasjon

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: '**.trycloudflare.com' }],
+  },
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
